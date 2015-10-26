3db7109dd468085d1e10d6887d6407047a2f9bab
Marco Fischersworring <mfiwo@web.de>
2015-10-26 23:22:56 +0000

    part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,42 @@
 
 def sum arr
   # YOUR CODE HERE
+  sum_val = 0
+  arr.each {|x| sum_val += x}
+  return sum_val
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  elsif arr.length == 2
+    return arr[0] + arr[1]
+  else
+    # sort from big to small
+    arr_s = arr.sort {|x,y| y <=> x }
+    return arr_s[0] + arr_s[1]
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if arr.length < 2
+    return FALSE
+  else
+    for i in 0...(arr.length - 1)
+      for j in (i + 1)...arr.length
+        #puts "i=#{i} j=#{j} #{arr[i]} + #{arr[j]}"
+        if arr[i] + arr[j] == n
+          #puts "TRUE"
+          return TRUE
+        end
+      end
+    end
+    return FALSE
+  end
 end
 
 # Part 2
